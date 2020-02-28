1af186dd4dbc82d293d5d3988582327957428c77
Paul <paul.bertrand@eleves.ec-nantes.fr>
2020-02-28 08:54:14 +0100

    Ajout d'un première texture

--- js/Arena.js
+++ js/Arena.js
@@ -6,12 +6,35 @@ Arena = function(game) {
     // Création de notre lumière principale
     var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 
-    // Créons une sphère 
-    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+    // Ajout d'un matériau
+    var materialGround = new BABYLON.StandardMaterial("groundTexture", scene);
+    materialGround.diffuseTexture = new BABYLON.Texture("assets/images/wood.jpg", scene);
+    materialGround.diffuseTexture.uScale = 4.0;
+    materialGround.diffuseTexture.vScale = 4.0;
 
-    // Remontons le sur l'axe y de la moitié de sa hauteur
-    sphere.position.y = 1;
+    // Le sol
+    var ground = BABYLON.Mesh.CreateGround("ground1", 20, 20, 2, scene);
+    ground.scaling = new BABYLON.Vector3(2, 10, 3);
+    ground.material = materialGround;
 
-    // Ajoutons un sol pour situer la sphere dans l'espace
-    var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+    // Les 4 cubes
+    var mainBox = BABYLON.Mesh.CreateBox("box1", 3, scene);
+    mainBox.scaling.y = 1;
+    mainBox.position = new BABYLON.Vector3(5, (3 / 2) * mainBox.scaling.y, 5);
+    mainBox.rotation.y = Math.PI * 45 / 180;
+    var mainBox2 = mainBox.clone("box2");
+    mainBox2.scaling.y = 2;
+    mainBox2.position = new BABYLON.Vector3(5, (3 / 2) * mainBox2.scaling.y, -5);
+    var mainBox3 = mainBox.clone("box3");
+    mainBox3.scaling.y = 3;
+    mainBox3.position = new BABYLON.Vector3(-5, (3 / 2) * mainBox3.scaling.y, -5);
+    var mainBox4 = mainBox.clone("box4");
+    mainBox4.scaling.y = 4;
+    mainBox4.position = new BABYLON.Vector3(-5, (3 / 2) * mainBox4.scaling.y, 5);
+
+    // Le cylindre
+    var cylinder = BABYLON.Mesh.CreateCylinder("cyl1", 20, 5, 5, 20, 4, scene);
+    cylinder.position.y = 20 / 2
+
+
 };
