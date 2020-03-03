bf168a5b5e2cab4487a8ccbcc85de217e8b0ca04
Paul <paul.bertrand@eleves.ec-nantes.fr>
2020-03-03 10:13:49 +0100

    Chargement du mesh test.babylon

--- js/Arena.js
+++ js/Arena.js
@@ -3,6 +3,28 @@ Arena = function(game) {
     this.game = game;
     var scene = game.scene;
 
+    BABYLON.SceneLoader.ImportMesh("", "assets/", "test.babylon", scene, function(newMeshes) {
+
+        // Explore tous les meshes disponibles et récupère le mesh nommé 'nameMesh'
+        for (var y = 0; y < newMeshes.length; y++) {
+            if (newMeshes[i].name === 'Cube') {
+
+                var searchMesh = newMeshes[i];
+                break;
+            }
+        };
+        // Convertit le mesh récupéré pour qu'il soit rendu en FlatShading
+        searchMesh.convertToFlatShadedMesh();
+
+        // On rend invisible notre mesh qui servira de prototype
+        searchMesh.isVisible = true;
+
+        // On rend le prototype accessible n'importe où
+        scene.searchMeshPrototype = searchMesh;
+
+    });
+
+    //searchMesh.position = new BABYLON.Vector3(0, 0, 0);
     // Création de notre lumière principale
     var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 
